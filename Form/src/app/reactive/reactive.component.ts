bce3576f607b3eb4a9201d9f8e2b82d5ade8c72b
Pradipkakde <kakde.pradip1@gmail.com>
2024-01-24 21:16:02 +0530

    chnages

--- Form/src/app/reactive/reactive.component.ts
+++ Form/src/app/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactive.component.css']
 })
 export class ReactiveComponent {
+  integerRegex=/^\d+$/;
+  emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  title='reactive-form';
+  registerForm=new FormGroup({
+    fname :new FormControl("",[Validators.required,Validators.maxLength(32)]),
+    lname :new FormControl("",[Validators.required,Validators.maxLength(32)]),
+    age   :new FormControl("",[Validators.required,Validators.max(60),Validators.min(18),Validators.pattern(this.integerRegex)]),
+    mobile   :new FormControl("",[Validators.required,Validators.max(10),Validators.min(10),Validators.pattern(this.integerRegex)]),
+  email   :new FormControl("",[Validators.required,Validators.max(32),Validators.min(32),Validators.pattern(this.emailRegex)]),
+password  :new FormControl("",[Validators.required,Validators.max(12),Validators.min(10)]),
+  confirmpassword  :new FormControl("",[Validators.required,Validators.max(12),Validators.min(10)]),
+  }) 
+  registerFn(){
+    console.log(this.registerForm.value);
+  }
 
 }
